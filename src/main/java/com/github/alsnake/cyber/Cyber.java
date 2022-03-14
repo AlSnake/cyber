9c352526e786e75ebd4b5a9fbbb29805db3a2b7c
AlSnake <notalsnake@gmail.com>
2022-03-14 18:07:10 -0400

    init commit

---
+++ src/main/java/com/github/alsnake/cyber/Cyber.java
@@ -0,0 +1,13 @@
+package com.github.alsnake.cyber;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Cyber {
+	private static final Logger LOGGER = LoggerFactory.getLogger(Cyber.class);
+
+    public static void main(String[] args) {
+        System.out.println("CYBER");
+		LOGGER.info("LOGGER");
+    }
+}
